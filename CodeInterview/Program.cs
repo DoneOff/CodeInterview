61a90d076a7af41286367fcdfb00ea7626596f8d
FeiFeiJin <dsf_jyf@qq.com>
2018-03-14 23:37:32 +0800

    1，添加vs code debugger 配置。 2，优化方法

--- CodeInterview/Program.cs
+++ CodeInterview/Program.cs
@@ -7,14 +7,18 @@ namespace CodeInterview
         {
             Console.WriteLine("测试开始");
             Chapter1_1();
-            Console.ReadLine();
-
         }
 
-        private static void Chapter1_1() {
-            Console.WriteLine("请输入校验字符串");
-            string str = Console.ReadLine();
-            Chapter1.Chapter1.isUniqueChar(str);
+        private static void Chapter1_1()
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入校验字符串，输入 exit 退出。");
+                string str = Console.ReadLine();
+                if(str.Equals("exit"))break;
+                Chapter1.Chapter1.isUniqueChar(str);
+            }
+
         }
     }
 }
