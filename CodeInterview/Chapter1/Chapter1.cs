eba6d7c8afece6c865ae8d7f90afb890fc6a79b1
FeiFeiJin <dsf_jyf@qq.com>
2018-03-14 22:31:33 +0800

    1，添加第一章习题

---
+++ CodeInterview/Chapter1/Chapter1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeInterview.Chapter1
+{
+    public static class Chapter1
+    {
+        /// <summary>
+        /// 判断字符串中是否出现重复字符
+        /// </summary>
+        /// <param name="str">需要判断的字符串，不区分中文</param>
+        /// <returns>是否包含重复字符</returns>
+        public static bool isUniqueChar(string str)
+        {
+            //用于存储key，可优化使用其他数据结构存储
+            Dictionary<char, bool> dic = new Dictionary<char, bool>();
+            string msg = "在“{0}”中，'{1}'重复出现！ ";
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = char.Parse(str.Substring(i, 1));
+                if (dic.ContainsKey(c))
+                {
+                    Console.WriteLine(string.Format(msg,str,c));
+                    return true;
+                }
+                else
+                {
+                    dic[c] = true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- CodeInterview/Program.cs
+++ CodeInterview/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
 namespace CodeInterview
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("测试开始");
+            Chapter1_1();
+            Console.ReadLine();
+
+        }
+
+        private static void Chapter1_1() {
+            Console.WriteLine("请输入校验字符串");
+            string str = Console.ReadLine();
+            Chapter1.Chapter1.isUniqueChar(str);
         }
     }
 }
